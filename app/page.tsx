cb88507c50459ba98e52ea76881a5aa904f79aab
Jessica Sartin <jessica.sartin@enolink.com>
2025-03-12 21:31:42 -0400

    about page some more

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image'
+import bdlCommunityPic from '../public/images/boston_community.png'
+
+const imageDescription = "Boston community enjoying a dodgeball event. Photo credit: Emily Hotz";
 
 export default function About() {
   return (
@@ -6,8 +9,8 @@ export default function About() {
         <h2 className="text-2xl font-bold mb-4">About Us</h2>
         <p>Welcome to the Boston Dodgeball League! We provide competitive, social dodgeball events for adults.</p>
         <div className="mt-4 text-center">
-          <Image src="/images/boston_community.png" alt="Boston community enjoying a dodgeball event" width={450} height={300} className="mx-auto" />
-            <p className="mt-2 text-sm"><em>Boston community enjoying a dodgeball event. <br />Photo credit: Emily Hotz</em></p>
+          <Image src={bdlCommunityPic} alt={imageDescription} className="mx-auto" />
+            <p className="mt-2 text-sm"><em>{imageDescription}</em></p>
         </div>
       </main>
   )
