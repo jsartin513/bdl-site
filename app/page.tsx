f2b8d229714df69d1867e5d1058166c6d84b06c9
Jessica Sartin <jessica.sartin@enolink.com>
2025-04-19 22:33:28 -0400

    Update description

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
   return (
       <main className="p-8">
         <h2 className="text-2xl font-bold mb-4">About Us</h2>
-        <p>Welcome to the Boston Dodgeball League! We provide competitive, social dodgeball events for adults.</p>
+        <p>Boston Dodgeball League was created by a group of local Boston dodgeballers. Our league is committed to providing a dynamic, inclusive, and competitive environment. We aim to foster teamwork, respect, and sportsmanship, while also enhancing community bonds through spirited competition and engaging events.</p>
         <div className="mt-4 text-center">
           <Image src={bdlCommunityPic} alt={imageDescription} className="mx-auto" />
             <p className="mt-2 text-sm"><em>{imageDescription}</em></p>
