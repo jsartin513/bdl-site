c740661e63945c5bd77fc3b38eb412305dd12b43
Jessica Sartin <jessica.sartin@enolink.com>
2025-04-25 17:21:15 -0400

    Add redirect from home to league

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
-import Image from 'next/image'
-import bdlCommunityPic from '../public/images/boston_community.png'
+import { redirect } from "next/navigation";
 
-const imageDescription = "Boston community enjoying a dodgeball event. Photo credit: Emily Hotz";
-
-export default function About() {
-  return (
-      <main className="p-8">
-        <h2 className="text-2xl font-bold mb-4">About Us</h2>
-        <p>Boston Dodgeball League was created by a group of local Boston dodgeballers. Our league is committed to providing a dynamic, inclusive, and competitive environment. We aim to foster teamwork, respect, and sportsmanship, while also enhancing community bonds through spirited competition and engaging events.</p>
-        <div className="mt-4 text-center">
-          <Image src={bdlCommunityPic} alt={imageDescription} className="mx-auto" />
-            <p className="mt-2 text-sm"><em>{imageDescription}</em></p>
-        </div>
-      </main>
-  )
+export default function Home() {
+  redirect("/league");
+  return null;
 }
