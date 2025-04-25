2236f97d1a0ce8fbe57a9a58f966455474113063
Jessica Sartin <jessica.sartin@enolink.com>
2025-04-25 14:12:42 -0400

    Add details panel

--- app/league/page.tsx
+++ app/league/page.tsx
@@ -1,11 +1,13 @@
 import LeagueStandings from "../components/LeagueStandings";
 import LeagueTeams from "../components/LeagueTeams";
+import LeagueDetails from "../components/LeagueDetails";
 
 export default function LeaguePage() {
   return (
-    <main className="p-8">
+    <main className="p-8 space-y-8">
+      <LeagueDetails />
       <LeagueStandings />
-      <LeagueTeams  />
+      <LeagueTeams />
       {/* Add more components or content here */}
     </main>
   );
